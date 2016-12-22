fb64799f6da4f90dc671f53acae41989948604a7
Annie <annie.mmt@gmail.com>
2016-12-22 15:10:57 -0500

    programmatic scrollview

--- AC3.2-Programmatic-Layout-Practice/AC3.2-Programmatic-Layout-Practice/ViewController.swift
+++ AC3.2-Programmatic-Layout-Practice/AC3.2-Programmatic-Layout-Practice/ViewController.swift
@@ -13,21 +13,38 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // 1. Add Scroll View 
+    self.view.backgroundColor = .blue
+    
+    // 1. Add Scroll View
+    self.view.addSubview(scrollView)
+    self.scrollView.translatesAutoresizingMaskIntoConstraints = false
     
     // 2. Add Scroll Constraints
+    self.edgesForExtendedLayout = []
+    
+    let _ = [
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8),
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8)
+        ].map { $0.isActive = true }
     
     // 3. Add View 
+    self.scrollView.addSubview(redView)
+    self.redView.translatesAutoresizingMaskIntoConstraints = false
     
     // 4. Add View Contraints
-    
-    // 5. Make it scroll
-    
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    let _ = [
+        redView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 2.0),
+        redView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 2.0),
+        
+        // 5. Make it scroll
+        redView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+        redView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+        
+        redView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+        redView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor)
+        ].map { $0.isActive = true }
   }
 
   lazy var scrollView: UIScrollView = {
@@ -35,14 +52,16 @@ class ViewController: UIViewController {
     view.backgroundColor = .green
     view.showsVerticalScrollIndicator = true
     view.showsHorizontalScrollIndicator = true
-    view.alwaysBounceHorizontal = true
-    view.alwaysBounceVertical = true
+    view.accessibilityIdentifier = "SCROLL"
+//    view.alwaysBounceHorizontal = true
+//    view.alwaysBounceVertical = true
     return view
   }()
   
   lazy var redView: UIView = {
     let view: UIView = UIView()
     view.backgroundColor = .red
+    view.accessibilityIdentifier = "RED"
     return view
   }()
 
